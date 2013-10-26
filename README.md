d13f04f2ed699bde4a5294328d1f7ae29dd22655
cynexit <me@cynexit.com>
2013-10-26 11:44:58 +0000

    first stepS

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-hackatum-td
+TUMdefense
 ===========
+
+T3h m0st 1337 def3nse out 7h3re! :D
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>TUMdefense</title>
+	<link rel="stylesheet" href="css/main.css">
+	<!-- vendor -->
+	<script type="text/javascript" src="js/vendor/jquery_min.js"></script>
+	<script type="text/javascript" src="js/vendor/kinetic_min.js"></script>
+	<!-- custom -->
+	<script type="text/javascript" src="js/engine.js"></script>
+	<script type="text/javascript" src="js/game.js"></script>
+</head>
+<body>
+	<div id="battlefield">
+	</div>
+</body>
+</html>
