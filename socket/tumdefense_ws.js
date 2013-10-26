3a96f36e0685d670dfb12f5d972d32d84bdd8857
cynexit <me@cynexit.com>
2013-10-26 19:58:56 +0000

    fixed socket server

--- socket/tumdefense_ws.js
+++ socket/tumdefense_ws.js
@@ -24,7 +24,7 @@ var server = http.createServer(function (request, response) {
 function connected_client(rid, request) {
 	this.id = rid;
 	this.state = 'online';
-	this.connection = request.accept(client_type, request.origin);
+	this.connection = request.accept(request.origin);
 
 	log_this('New connected client, IP: '+this.connection.remoteAddress);
 
