3fc6b250ae4d23c0401b668b40aa5660920eafc4
AlPotapov <herr.potapov@googlemail.com>
2013-10-26 22:31:01 +0200

    Victory

---
+++ js/castle.js
@@ -0,0 +1,25 @@
+var health_1 = 100;
+var health_2 = 100;
+
+function health_subtract(base, change) {
+	if(base == 1) {
+	  health_1 -= change;
+	  if(health_1 <= 0){
+  	  health_1 = 0;
+  	  return true
+	  }else{
+  	  return false;
+	  }
+  }
+  
+	if(base == 2) {
+	  health_2 -= change;
+	  if(health_2 <= 0){
+  	  health_2 = 0;
+  	  return true
+	  }else{
+  	  return false;
+	  }
+  }
+	console.log("base: " + base + "; change: " + change);
+}
